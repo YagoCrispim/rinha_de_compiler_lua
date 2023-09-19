5ad427f3e7a580133eabbd812f5b2bd70baf8d31
Yago Crispim <34103098+YagoCrispim@users.noreply.github.com>
2023-09-19 18:15:24 -0300

    feat: improvements

--- src/symtab.lua
+++ src/symtab.lua
@@ -1,35 +1,5 @@
 local class = require "lib.class"
-
-local Scope = class({
-  constructor = function (self, params)
-    self.name = params.name
-    self.level = params.level
-    self.parentScope = params.parentScope
-    self.symbols = {}
-  end,
-  methods = {
-    lookup = function (self, name)
-      local symbol = self.symbols[name]
-      if symbol then
-        return symbol
-      end
-      if self.parentScope then
-        return self.parentScope:lookup(name)
-      end
-      return nil
-    end,
-
-    define = function (self, name, value)
-        self.symbols[name] = value
-    end,
-
-    _log = function (self, ...)
-      if self._allowLog then
-        print('[SymTab]: ' .. ...)
-      end
-    end
-  }
-})
+local Scope = require "src.scope"
 
 local SymbolTable = class({
   constructor = function (self)
